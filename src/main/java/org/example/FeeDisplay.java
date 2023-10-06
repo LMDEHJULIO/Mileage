1ce19266373b9387ab0744c6e0735be38de93c2b
Julio Rodriguez <juliodevelops@gmail.com>
2023-10-06 06:48:11 -0400

    Working draft of FeeDisplay

---
+++ src/main/java/org/example/FeeDisplay.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import javax.sound.midi.Soundbank;
+import java.sql.SQLOutput;
+
+public class FeeDisplay {
+   static String critera1 = "0 - 5";
+   static String critera2 = "6 - 15";
+   static String critera3 = "16 - 25";
+   static String critera4 = "26 - 50";
+   static String criteria5 = "More than 5";
+
+    static int fee1 = 2;
+    static int fee2 = 5;
+    static int fee3 = 10;
+    static int fee4 = 15;
+    static int fee5 = 20;
+
+    // need to input params backwards for the formatting
+    //    https://stackoverflow.com/questions/19961671/java-creating-blank-spaces-with-printf
+
+    public static void display(){
+        System.out.printf("%20s = Fee\n", "Distance");
+        System.out.printf("%23s" + " = $" + fee1 + "\n", critera1 + " miles ");
+        System.out.printf("%23s" + " = $" + fee2 + "\n", critera2 + " miles ");
+        System.out.printf("%23s" + " = $" + fee3 + "\n", critera3 + " miles ");
+        System.out.printf("%23s" + " = $" + fee4 + "\n", critera4 + " miles ");
+        System.out.printf("%23s" + " = $" + fee5 + "\n", criteria5 + " miles ");
+
+    }
+
+    public static void main(String[] args) {
+        FeeDisplay.display();
+    }
+}
---
+++ src/main/java/org/example/TotalDisplay.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TotalDisplay {
+//    Map<String, String> critera1 = new HashMap<String, String>();
+
+
+
+
+}
