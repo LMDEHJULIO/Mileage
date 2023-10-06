e21ee02369426a4ed3157c82360543d88c5e99e4
Julio Rodriguez <juliodevelops@gmail.com>
2023-10-05 22:58:20 -0400

    Get prompts working finally

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
         MileageScanner mileageSurvey = new MileageScanner();
 
-        mileageSurvey.promptUser(mileageSurvey.prompts.get(1));
+        mileageSurvey.runSurvey();
+
     }
 }
--- src/main/java/org/example/MileageScanner.java
+++ src/main/java/org/example/MileageScanner.java
@@ -8,30 +8,56 @@ import java.util.function.Supplier;
 public class MileageScanner {
     Scanner scanner = new Scanner(System.in);
     //Map the prompt to the scanner input
-    Map<String, PromptMethod> prompts = new HashMap<>();
+    Map<PromptEnum, Supplier<Object>> prompts = new HashMap<>();
+
+    public enum PromptEnum {
+        QUANTITY("Enter the quantity: "),
+        PRICE("Enter the price: "),
+        DELIVERY_DISTANCE("Enter the delivery distance (miles): ");
+
+        private String prompt;
+//        https://www.w3schools.com/java/java_enums.asp
+        PromptEnum(String prompt){
+           this.prompt = prompt;
+        }
+
+        public String getPrompt() {
+            return prompt;
+        }
+
+        public void setPrompt(String prompt) {
+            this.prompt = prompt;
+        }
+    }
 
     interface PromptMethod {
         void execute();
     }
 
     public MileageScanner() {
-        prompts.put("Enter the product name: ", () -> scanner.nextLine());
-        prompts.put("Enter the quantity: ", () -> scanner.nextInt());
-        prompts.put("Enter the price per item: ", () -> scanner.nextDouble());
-        prompts.put("Enter the delivery distance (miles): ", () -> scanner.nextDouble());
+        prompts.put(PromptEnum.QUANTITY, () -> scanner.nextInt());
+        prompts.put(PromptEnum.PRICE, () -> scanner.nextDouble());
+        prompts.put(PromptEnum.DELIVERY_DISTANCE, () -> scanner.nextDouble());
     }
 
 //    https://www.w3schools.blog/map-interface-in-java
 
-    public Map<String, PromptMethod> getPrompts() {
+    public Map<PromptEnum, Supplier<Object>> getPrompts() {
         return prompts;
     }
 
-    public void setPrompts(Map<String, Supplier<Object>> prompts) {
+    public void setPrompts(Map<PromptEnum, Supplier<Object>> prompts) {
         this.prompts = prompts;
     }
 
-    public Object promptUser(String prompt){
+    public Object promptUser(PromptEnum prompt){
+        System.out.println(prompt.getPrompt());
         return prompts.get(prompt).get();
+    }
+
+    public void runSurvey(){
+        this.promptUser(MileageScanner.PromptEnum.QUANTITY);
+        this.promptUser(MileageScanner.PromptEnum.PRICE);
+        this.promptUser(MileageScanner.PromptEnum.DELIVERY_DISTANCE);
     }
 }
